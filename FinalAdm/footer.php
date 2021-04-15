38e0444eb5f510dc43e6f905d01997c6ac10de63
Juan David Matos <Juand0014@hotmail.com>
2021-04-15 13:50:55 -0400

    modificacion de algunas vista del administrador

--- FinalAdm/footer.php
+++ FinalAdm/footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Sweet Happineess</title>
-    <style>
+<style>
         .footer{
             margin-top:20px;
             background: #284243;
@@ -22,8 +14,6 @@
             margin: 20px;
         }
     </style>
-</head>
-<body>
 <div class="footer text-center">
                <div class="img-1 f">
                    <img src="img/escudo.png" alt="Escudo Dominicano">
--- FinalAdm/gestionarEmpleados.php
+++ FinalAdm/gestionarEmpleados.php
@@ -1,4 +1,4 @@
- <?php include("head.php");
+ <?php 
  
     if(isset($_GET['del'])){
 
@@ -9,56 +9,16 @@
         $sql2 = "INSERT INTO log_huesped(usuario_operario, usuario_afectado, accion, fecha_hora) VALUES ('Administrador', 'Empleado', 'Eliminó Empleado', '{$fecha}')";
         $rs = conexion::execute($sql2);
     }
- ?>
+include('redes.php');
 
-<div class="container" style="max-width: 90%; margin-top:50px;">
-    <h2>Empleados</h2>
-     <a href="agregarEmpleado.php" class="btn btn-secondary" style="margin-top:25px; width:250px;color:white;">Añadir Empleado</a>
-</div>
-<div style="max-width: 90%; margin:50px auto; margin-bottom: 250px;">
-       <table class="table table-hover">
-       <thead>
-           <tr>
-               <th scope="col">Cédula</th>
-               <th scope="col">Nombre</th>
-               <th scope="col">Apellido</th>
-               <th scope="col">Teléfono</th>
-               <th scope="col">Celular</th>
-               <th scope="col">Email</th>
-               <th scope="col">Rol</th>
-           </tr>
-       </thead>
-       <tbody>
-           <?php 
-               $sql = "SELECT * FROM empleados";
-               $rs = conexion::query_array($sql);
+?>
+<!--end menu-h-->
 
-               foreach($rs as $data)
-               {
-                   echo "
-                      <tr>
-                         <td>{$data['cédula']}</td>
-                         <td>{$data['nombre']}</td>
-                         <td>{$data['apellido']}</td>
-                         <td>{$data['teléfono']}</td>
-                         <td>{$data['celular']}</td>
-                         <td>{$data['correo']}</td>
-                         <td>{$data['rol_hotel']}</td>
-                         <td>
-                            <a class='btn btn-danger'href='gestionarEmpleados.php?del={$data['id']}' style='margin-right: 20px; width:100px;color:white;'>Eliminar</a>
-                            <a href='agregarEmpleado.php?edit={$data['id']}' class='btn btn-success' style='width:100px;color:white;'>Editar</a>
-                         </td>
-                      </tr>
-                   ";
-               }
-           
-           ?>
-       </tbody>
-       </table>
-</div>
+
 
 
 <?php include("footer.php")?>
+
 
 
 
