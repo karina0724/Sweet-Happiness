def93af5469cc51f3d04eea6b174cc7f45ae22cc
Juan David Matos <Juand0014@hotmail.com>
2021-04-15 14:16:57 -0400

    otra modificacion

--- FinalAdm/gestionarSucursales.php
+++ FinalAdm/gestionarSucursales.php
@@ -1,6 +1,5 @@
 <?php 
 ob_start();
-include("head.php");
 
 $fecha= (new DateTime())->format('Y/m/d H:i:s'); 
  if(isset($_GET['del'])){
@@ -50,23 +49,22 @@ $fecha= (new DateTime())->format('Y/m/d H:i:s');
 }
 ob_end_flush();
 ?>
+<div style="max-width: 80%;" class="container">
+    <?php include('head.php'); ?> <br> <br>
 
-<div class="container" style="max-width: 90%; margin-top:50px;">
- <h2>Sucursales</h2>
- <button data-toggle='modal'data-target='#modalRegistrarSucursal' class='btn btn-secondary' style="margin-top:25px; width:250px;color:white;">Nueva Sucursal</button>
-
-</div>
-<div style="max-width: 90%; margin:50px auto; margin-bottom: 250px;">
-    <table class="table table-hover">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Provincia</th>
-            <th scope="col">Dirección</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php 
+    <div class="container col-md-7">
+        <h2>Sucursales</h2>
+        <button data-toggle='modal' data-target='#modalRegistrarSucursal' class='btn btn-secondary'style="width:250px;color:white;">Nueva Sucursal</button>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Provincia</th>
+                    <th scope="col">Dirección</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
             $sql = "SELECT * FROM sucursales";
             $rs = conexion::query_array($sql);
 
@@ -86,67 +84,79 @@ ob_end_flush();
             }
         
         ?>
-    </tbody>
-    </table>
+            </tbody>
+        </table>
 
-     <!-- Vertically centered insert modal -->
-     <div class="modal" tabindex="-1" role="dialog" id="modalRegistrarSucursal" aria-labelledby="titulo-modal2" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                <div class="foto">
-                  <img src="img/logo.png" class="logo-modal" alt="Sweet Happineess">
+        <!-- Vertically centered insert modal -->
+        <div class="modal" tabindex="-1" role="dialog" id="modalRegistrarSucursal" aria-labelledby="titulo-modal2"
+            aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="foto">
+                            <img src="img/logo.png" class="logo-modal" alt="Sweet Happineess">
+                        </div>
+                        <form method="POST" class="mb-2">
+                            <div class="form-group">
+                                <label for="password" class="font-weight-bold">Dirección</label>
+                                <input type="text" class="form-control" id="direccion"
+                                    value="<?php echo $_POST['direccion']?>" placeholder="Dirección" name="direccion">
+                            </div>
+                            <div class="form-group">
+                                <label for="provincia" class="font-weight-bold">Provincia</label>
+                                <input type="text" class="form-control" id="provincia"
+                                    value="<?php echo $_POST['provincia']?>" placeholder="Provincia" name="provincia">
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn col-12" style="margin: 5px auto;">Guardar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <form method="POST" class="mb-2">
-                    <div class="form-group">
-                        <label for="password" class="font-weight-bold">Dirección</label>
-                        <input type="text" class="form-control" id="direccion" value="<?php echo $_POST['direccion']?>" placeholder="Dirección" name="direccion">
-                    </div>
-                    <div class="form-group">
-                        <label for="provincia" class="font-weight-bold">Provincia</label>
-                        <input type="text" class="form-control" id="provincia" value="<?php echo $_POST['provincia']?>" placeholder="Provincia" name="provincia">
-                    </div>
-                    <div class="text-center">
-                        <button type="submit" class="btn col-12" style="margin: 5px auto;">Guardar</button>
-                    </div>
-                </form>    
-              </div>
             </div>
-        </div>
-      <!-- End Vertically centered login modal -->
+            <!-- End Vertically centered login modal -->
 
 
-       <!-- Vertically centered update modal -->
-     <div class="modal" tabindex="-1" role="dialog" id="modalEditarSucursal" aria-labelledby="titulo-modal2" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                <div class="foto">
-                  <img src="img/logo.png" class="logo-modal" alt="Sweet Happineess">
+            <!-- Vertically centered update modal -->
+            <div class="modal" tabindex="-1" role="dialog" id="modalEditarSucursal" aria-labelledby="titulo-modal2"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="foto">
+                                <img src="img/logo.png" class="logo-modal" alt="Sweet Happineess">
+                            </div>
+                            <form method="POST" class="mb-2">
+                                <div class="form-group">
+                                    <label for="password" class="font-weight-bold">Dirección</label>
+                                    <input type="text" class="form-control" id="direccion"
+                                        value="<?php echo $_POST['direccion']?>" placeholder="Dirección"
+                                        name="direccion">
+                                </div>
+                                <div class="form-group">
+                                    <label for="provincia" class="font-weight-bold">Provincia Edi</label>
+                                    <input type="text" class="form-control" id="provincia"
+                                        value="<?php echo $_POST['provincia']?>" placeholder="Provincia"
+                                        name="provincia">
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn col-12" style="margin: 5px auto;">Guardar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <form method="POST" class="mb-2">
-                    <div class="form-group">
-                        <label for="password" class="font-weight-bold">Dirección</label>
-                        <input type="text" class="form-control" id="direccion" value="<?php echo $_POST['direccion']?>" placeholder="Dirección" name="direccion">
-                    </div>
-                    <div class="form-group">
-                        <label for="provincia" class="font-weight-bold">Provincia Edi</label>
-                        <input type="text" class="form-control" id="provincia" value="<?php echo $_POST['provincia']?>" placeholder="Provincia" name="provincia">
-                    </div>
-                    <div class="text-center">
-                        <button type="submit" class="btn col-12" style="margin: 5px auto;">Guardar</button>
-                    </div>
-                </form>    
-              </div>
+                <hr>
             </div>
+
         </div>
-      <!-- End Vertically centered edit login modal -->
+    </div>
+    <!--end contacto-->
 </div>
-<script>
-  
-</script>
+</div>
+</div>
+<!--end bg1-->
+
+</body>
 
 <?php include("footer.php")?>
-
-
-
