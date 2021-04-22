7df28e28bbcb53d307a07bfcc22a109f3fa78c0e
Karina Montero <karina_0711_@hotmail.com>
2021-04-21 20:14:32 -0400

    terminando

--- FinalAdm/ReservarHabitaciones.php
+++ FinalAdm/ReservarHabitaciones.php
@@ -2,27 +2,51 @@
 ob_start();
 include('header.php'); 
 
+if($_POST){
+    
+    extract($_POST);
+
+    if(isset($_GET['edit'])){
+
+        $sql= "SELECT costo FROM habitaciones WHERE categoria = {$_POST['habitacion']}"; 
+        $rs = conexion::query_array($sql);
+        $precio = 100;
+         var_dump($_POST);
+         exit();
+
+        $sql2="UPDATE reservaciones_habitaciones SET id_habitaciones={$idHabitacion}, id_sucursales={$provincia}, fecha_inicio='{$fechaIni}', fecha_fin= '{$fechaFin}', monto_total={$precio} WHERE id={$_GET['edit']}";
+    
+        $rs = conexion::execute($sql2); 
+    }else{ 
+        $sql2 = "INSERT INTO reservaciones_habitaciones(id_habitaciones, id_HReservador, id_sucursales, fecha_inicio, fecha_fin, monto_total) 
+        VALUES ({$idHabitacion}, {$_SESSION['UserId']}, {$provincia}, '{$fechaIni}', '{$fechaFin}', {$precio})";
+    
+        $rs = conexion::execute($sql2); 
+    }
+
+    if($rs){
+        header("location: consultarReservaciones.php?idHuesped={$_SESSION['UserId']}");
+    } 
+   
+}
+
 if(isset($_GET['precio']) && isset($_GET['habitacion']) && isset($_GET['idHabitacion'])){
     $precio = $_GET['precio'];
-    $porciento = $precio / 10;
     $tipoHabitacion = $_GET['habitacion'];
     $idHabitacion = $_GET['idHabitacion'];
 }
 
+if(isset($_GET['edit'])){
+    $sql= "select * from reservaciones_habitaciones where id= {$_GET['edit']}";
+    $rs = conexion::query_array($sql);
 
-if($_POST){
+    if(count($rs) > 0){
+        $data = $rs[0];
+        $_POST= $data;  
+    }
     
-    extract($_POST);
-    var_dump($idHabitacion, $_SESSION['UserId'], $provincia, $fechaIni, $fechaFin,  $precio);
-   
-    $sql2 = "INSERT INTO reservaciones_habitaciones(id_habitaciones, id_HReservador, id_sucursales, fecha_inicio, fecha_fin, monto_total) 
-    VALUES ({$idHabitacion}, {$_SESSION['UserId']}, {$provincia}, '{$fechaIni}', '{$fechaFin}', {$precio})";
-
-    $rs = conexion::execute($sql2);
-
-    header("location: consultarReservaciones.php?idHuesped={$_SESSION['UserId']}");
-   
 }
+
 if(isset($_SESSION['initialization_sistem'])){
     if(isset($_SESSION['rol']) == 3 || isset($_SESSION['rol']) == 1){
 
@@ -31,26 +55,68 @@ ob_end_flush();
 <br>
 <div class=" container col-md-8">
     <h2>Reservar Habitación</h2>
-    <form method="GET" method="POST">
+    <form method="POST">
         <div class="form-row">
         <div class="form-group col-md-6">
-                        <label for="habitacion">Habitación</label>   
-                        <input type="text" class="form-control" id="habitacion" name="habitacion" value="<?php echo "$tipoHabitacion" ?>" disabled>
+                        <label for="habitacion">Habitación</label> 
+                        <?php 
+                          if(isset($_GET['edit'])){
+                           $sql= "SELECT * FROM habitaciones WHERE id = {$_POST['id_habitaciones']}"; 
+                           $rs = conexion::query_array($sql);?>
+
+                           <select name="habitacion" class="form-control" id="habitacion">
+                          <?php foreach($rs as $data)
+                           {
+                              echo "
+                                 <option value='{$data['id']}' selected>{$data['categoria']}</option>
+                              ";
+                           }
+
+                           $sql2= "SELECT * FROM habitaciones WHERE id != {$_POST['id_habitaciones']}"; 
+                           $rs2 = conexion::query_array($sql2);
+                           foreach($rs2 as $data)
+                           {
+                              echo "
+                                 <option value='{$data['id']}'>{$data['categoria']}</option>
+                              ";
+                           }?>
+                            </select>
+                           <?php
+
+                        }    
+                        else{?>
+                            <input type="text" class="form-control" id="habitacion" name="habitacion" value="<?php echo "$tipoHabitacion"; ?>" disabled>
+
+                            <?php
+                            } 
+                        ?>  
         </div>
             <div class="form-group col-md-6">
                         <label for="provincia">Sucursal</label>
                             <select name="provincia" class="form-control" id="provincia" name="provincia" required>
                             <?php 
                                if(isset($_GET['edit'])){
-                                   ?> <option value=<?php echo $_POST['id_sucursales']; ?>><?php echo $_POST['id_sucursales'];?></option><?php
-                                   $sql = "SELECT * FROM sucursales WHERE provincia != '{$_POST['id_sucursales']}'";
-                                   $rs = conexion::query_array($sql);
-                                   foreach($rs as $data)
-                                   {
-                                      echo "
-                                         <option value='{$data['id']}'>{$data['provincia']}</option>
-                                      ";
-                                   }
+                                   
+                           $sql= "SELECT * FROM sucursales WHERE id = '{$_POST['id_sucursales']}'"; 
+                           $rs = conexion::query_array($sql);
+
+                           foreach($rs as $data)
+                           {
+                              echo "
+                                 <option value='{$data['id']}' selected>{$data['provincia']}</option>
+                              ";
+                           }
+
+                           $sql2= "SELECT * FROM sucursales WHERE id != '{$_POST['id_sucursales']}'"; 
+                           $rs2 = conexion::query_array($sql2);
+
+                           foreach($rs2 as $data)
+                           {
+                              echo "
+                                 <option value='{$data['id']}'>{$data['provincia']}</option>
+                              ";
+                           }
+
                                }
                                else{
                                 ?> <option value=0>Seleccione una opción</option><?php
@@ -70,15 +136,27 @@ ob_end_flush();
         </div>
         <div class="form-group">
             <label for="fechaIni">Fecha de inicio</label>
-            <input type="date" class="form-control" id="fechaIni" name="fechaIni" placeholder="Fecha Inicio">
+            <input type="date" class="form-control" id="fechaIni" name="fechaIni" value="<?php echo $_POST['fecha_inicio']; ?>">
         </div>
         <div class="form-group">
             <label for="fechaFin">Fecha Final</label>
-            <input type="date" class="form-control" id="fechaFin" name="fechaFin" placeholder="Fecha Final">
+            <input type="date" class="form-control" id="fechaFin" name="fechaFin" value="<?php echo $_POST['fecha_fin']; ?>" >
         </div>
         <div class="form-group">
-            <label for="precio">Precio</label>
-            <input type='number' class='form-control' id='precio' name='precio' value="<?php echo "$porciento" ?>" disabled>
+            
+            <?php 
+               if(isset($_GET['edit'])){ 
+            ?>
+                <input type='number' class='form-control' id='precio' name='precio' hidden>              
+           
+            <?php
+            }else{
+            ?>
+                   <label for="precio">Precio</label>
+                   <input type='number' class='form-control' id='precio' name='precio' value="<?php echo "$precio"; ?>" disabled>
+            <?php
+            }
+            ?>
 
         </div>
         <div class="text-center">
--- FinalAdm/consultarReservaciones.php
+++ FinalAdm/consultarReservaciones.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
     include('header.php');
     include('redes.php');
 
@@ -6,6 +7,15 @@
     {
         $idHuesped = $_GET['idHuesped'];
     }
+    if(isset($_GET['del']))
+    {
+        $idRHuesped = $_GET['del'];
+        $sql = "delete from reservaciones_habitaciones where id = {$idRHuesped}";
+        $rs = conexion::execute($sql);
+
+        header("location: consultarReservaciones.php?idHuesped={$_GET['idHuesped']}");
+    } 
+ob_end_flush();
 ?>
 <!--end menu-h-->
 
@@ -15,7 +25,7 @@
 <a href="consultarHabitaciones.php" class="btn btn-secondary" style="margin-top:20px; width:250px;color:white;">Agregar Reservación</a><br><br><br>
 
 <?php 
-    $sql = "SELECT r.id r.fecha_inicio, DATEDIFF(fecha_fin, fecha_inicio) as Dias, s.provincia, r.monto_total from reservaciones_habitaciones r inner join sucursales s  on s.id = r.id_sucursales where r.id_HReservador = {$idHuesped}";
+    $sql = "SELECT r.id, r.fecha_inicio, DATEDIFF(fecha_fin, fecha_inicio) as Dias, s.provincia, r.monto_total from reservaciones_habitaciones r inner join sucursales s  on s.id = r.id_sucursales where r.id_HReservador = {$idHuesped}";
     $rs = conexion::query_array($sql);
     
     if(!$rs){
@@ -47,13 +57,12 @@
                                         <td>{$data['provincia']}</td>
                                         <td>RD$ {$data['monto_total']}</td>
                                         <td>
-                                           <a class= 'btn btn-info'>Ver detalles</a>
-                                           <a href='ReservarHabitaciones.php' class= 'btn btn-success'>
+                                           <a href='ReservarHabitaciones.php?edit={$data['id']}' class= 'btn btn-success'>
                                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
                                                 <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/>
                                             </svg>
                                            </a>
-                                           <a href='consultarReservaciones.php' class= 'btn btn-danger'>
+                                           <a href='consultarReservaciones.php?idHuesped={$_GET['idHuesped']}&del={$data['id']}' class= 'btn btn-danger'>
                                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill'viewBox='0 0 16 16'>
                                             <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
                                             </svg>
@@ -77,5 +86,3 @@
 </body>
 <?php include("footer.php")?>
 </html>
-
-
